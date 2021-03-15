eebeacb214d26c33de0137e212675aee004724c8
AakashRaina <aakashraina9@gmail.com>
2021-03-15 17:04:01 +0530

    Refactor compose form

--- src/js/components/ComposeModal.js
+++ src/js/components/ComposeModal.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import "css/ComposeModal.styl";
 import { AiFillCloseCircle } from "react-icons/ai";
 
 function ComposeModal({ showCompose, toggleComposeModal }) {
-  const [to, setto] = useState(null);
-  const [cc, setcc] = useState(null);
-  const [subject, setsubject] = useState(null);
-  const [body, setbody] = useState(null);
+  const send = (e) => {
+    e.preventDefault();
 
-  const isSendDisabled = () => {
-    if (to && subject && body) return false;
-    else return true;
-  };
+    const to = e.target[0].value;
+    const cc = e.target[1].value;
+    const subject = e.target[2].value;
+    const body = e.target[3].value;
 
-  const send = () => {
+    if (!to || !subject || !body) return;
+
     const sentItems = JSON.parse(localStorage.getItem("sentItems"));
-
     const sentMail = {
       id: `${Math.random() * (40 - 30) + 30}`,
       status: "READ",
       from: "aakashraina9@gmail.com",
       to: [`${to}`],
-      cc: cc ? [`${cc}`] : undefined,
       subject: `${subject}`,
       body: `${body}`,
       date: "2/12/21",
     };
+
+    if (cc) {
+      sentMail.cc = [`${cc}`];
+    }
 
     if (sentItems) {
       sentItems.push(sentMail);
@@ -33,16 +34,16 @@ function ComposeModal({ showCompose, toggleComposeModal }) {
     } else {
       localStorage.setItem("sentItems", JSON.stringify([sentMail]));
     }
-
     toggleComposeModal();
   };
 
   if (!showCompose) return null;
 
   return (
-    <div
+    <form
       class='flex flex-col absolute bottom-0 right-0 w-1/2 h-1/2 compose-container border bg-white .shadow-md mb-3 mr-3 rounded-lg'
       style={{ height: "50%" }}
+      onSubmit={send}
     >
       <div class='flex justify-end items-center pr-2 border-b border-gray-300'>
         <span onClick={toggleComposeModal} class='cursor-pointer'>
@@ -50,42 +51,31 @@ function ComposeModal({ showCompose, toggleComposeModal }) {
         </span>
       </div>
       <div class='border-b border-gray-300'>
-        <input
-          class='h-full w-full pl-2'
-          placeholder='To'
-          onBlur={(e) => setto(e.target.value)}
-        />
+        <input class='h-full w-full pl-2' placeholder='To' name='to' />
+      </div>
+      <div class='border-b border-gray-300'>
+        <input class='h-full w-full pl-2' placeholder='Cc' name='cc' />
       </div>
       <div class='border-b border-gray-300'>
         <input
           class='h-full w-full pl-2'
-          placeholder='Cc'
-          onBlur={(e) => setcc(e.target.value)}
-        />
-      </div>
-      <div class='border-b border-gray-300'>
-        <input
-          class='h-full w-full pl-2'
-          placeholder='subject'
-          onBlur={(e) => setsubject(e.target.value)}
+          placeholder='Subject'
+          name='subject'
         />
       </div>
       <div class='border-b border-gray-300'>
         <textarea
-          class='h-full w-full resize-none'
-          onBlur={(e) => setbody(e.target.value)}
+          class='h-full w-full resize-none pl-2'
+          name='body'
+          placeholder='Enter your body here'
         />
       </div>
       <div class='flex justify-end items-center'>
-        <button
-          class='bg-blue-300 p-2 rounded'
-          onClick={send}
-          disabled={isSendDisabled()}
-        >
+        <button class='bg-blue-300 p-2 rounded' type='submit'>
           Send
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
